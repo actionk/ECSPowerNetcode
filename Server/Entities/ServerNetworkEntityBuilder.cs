412a21731f070f907759762f6ad9275461a22609
Andrey Cherkashin <a.cherkashin@mytaxi.com>
2020-05-18 18:13:26 +0200

    + server entity builder

---
+++ Server/Entities/ServerNetworkEntityBuilder.cs
@@ -0,0 +1,14 @@
+using Plugins.ECSEntityBuilder;
+using Plugins.ECSPowerNetcode.Shared;
+
+namespace Plugins.ECSPowerNetcode.Server.Entities
+{
+    public abstract class ServerNetworkEntityBuilder<T> : EntityBuilder<T>
+    {
+        protected override void OnPreBuild(EntityManagerWrapper wrapper)
+        {
+            var networkEntityId = ServerManager.Instance.NextNetworkEntityId;
+            AddComponentData(new NetworkEntity {networkEntityId = networkEntityId});
+        }
+    }
+}
