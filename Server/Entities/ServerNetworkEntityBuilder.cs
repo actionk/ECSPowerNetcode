9e663a85474e8530373d7c058d867285992a136f
Andrey Cherkashin <a.cherkashin@mytaxi.com>
2020-05-28 21:45:27 +0200

    + networkEntityId as property

--- Server/Entities/ServerNetworkEntityBuilder.cs
+++ Server/Entities/ServerNetworkEntityBuilder.cs
@@ -6,10 +6,12 @@ namespace Plugins.ECSPowerNetcode.Server.Entities
 {
     public abstract class ServerNetworkEntityBuilder<T> : EntityBuilder<T> where T : EntityBuilder<T>
     {
+        public ulong NetworkEntityId { get; }
+
         protected ServerNetworkEntityBuilder()
         {
-            var networkEntityId = ServerManager.Instance.NextNetworkEntityId;
-            AddComponentData(new NetworkEntity {networkEntityId = networkEntityId});
+            NetworkEntityId = ServerManager.Instance.NextNetworkEntityId;
+            AddComponentData(new NetworkEntity {networkEntityId = NetworkEntityId});
         }
 
         protected ServerNetworkEntityBuilder(ulong networkEntityId)
