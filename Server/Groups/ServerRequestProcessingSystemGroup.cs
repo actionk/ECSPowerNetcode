c57ea05091c5c3a76438573c2517df798e76bb79
Andrey Cherkashin <action.open@gmail.com>
2020-10-21 19:55:13 +0200

    + adding quad trees

--- Server/Groups/ServerRequestProcessingSystemGroup.cs
+++ Server/Groups/ServerRequestProcessingSystemGroup.cs
@@ -3,7 +3,7 @@ using Unity.NetCode;
 
 namespace Plugins.ECSPowerNetcode.Server.Groups
 {
-    [UpdateInGroup(typeof(ServerSimulationSystemGroup))]
+    [UpdateInGroup(typeof(Unity.NetCode.ServerSimulationSystemGroup))]
     [UpdateAfter(typeof(ServerConnectionSystemGroup))]
     public class ServerRequestProcessingSystemGroup : ComponentSystemGroup
     {
