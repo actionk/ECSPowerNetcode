2ade88ac3049ce7045d5e08c65f4a96a38c120a4
Andrey Cherkashin <action.open@gmail.com>
2020-10-30 16:29:43 +0100

    + system group order fix

--- Client/Groups/ClientNetworkEntitySystemGroup.cs
+++ Client/Groups/ClientNetworkEntitySystemGroup.cs
@@ -1,11 +1,9 @@
 using Unity.Entities;
 using Unity.NetCode;
-using Unity.Transforms;
 
 namespace Plugins.ECSPowerNetcode.Client.Groups
 {
     [UpdateInGroup(typeof(ClientInitializationSystemGroup))]
-    [UpdateBefore(typeof(TransformSystemGroup))]
     public class ClientNetworkEntitySystemGroup : ComponentSystemGroup
     {
     }
