9d9c8c10062330882a71f91c44875c1a088f8858
Andrey Cherkashin <action.open@gmail.com>
2020-11-03 19:07:02 +0100

    + more extensions

---
+++ Extensions/CollectionExtension.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Unity.Collections;
+
+namespace Plugins.ECSPowerNetcode.Extensions
+{
+    public static class CollectionExtension
+    {
+        public static FixedListInt512 ToFixedListInt512(this IEnumerable<int> array)
+        {
+            var list = new FixedListInt512();
+            foreach (var entry in array)
+                list.AddNoResize(entry);
+            return list;
+        }
+    }
+}
