e1e680b1972e45883d590c6f034ecf741fade34c
Andrey Cherkashin <action.open@gmail.com>
2020-11-09 14:56:47 +0100

    + some refactoring

--- Extensions/TimeExtensions.cs
+++ Extensions/TimeExtensions.cs
@@ -5,11 +5,16 @@ namespace Plugins.ECSPowerNetcode.Extensions
 {
     public static class TimeExtensions
     {
-        public static ulong ElapsedTimeMs(this TimeData timeData)
+        public static ulong ElapsedTimeInMillis(this TimeData timeData)
         {
             return (ulong) (timeData.ElapsedTime * 1000);
         }
 
-        public static ulong FixedTimeMs => (ulong) (Time.fixedTime * 1000);
+        public static uint ElapsedTimeInSeconds(this TimeData timeData)
+        {
+            return (uint) timeData.ElapsedTime;
+        }
+
+        public static ulong FixedTimeInMillis => (ulong) (Time.fixedTime * 1000);
     }
 }
